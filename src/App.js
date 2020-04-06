b4eb48409252f8a9fa31b8836c06e75e4e3b0b08
Florian Weiss <florian_magna@hotmail.com>
2020-04-07 00:27:46 +0200

    add routes and standard design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Nav, Navbar } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
 import './App.css';
 
-function App() {
+import Home from './components/Home/Home';
+import World from './components/World/World';
+
+function App () {
+  /*const client = new ApolloClient({
+    uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  });*/
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/*<ApolloProvider client={client}>*/}
+        <Router>
+          <Navbar>
+            <Navbar.Brand href="/">IM Datentool</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="/world">Weltübersicht</Nav.Link>
+                <Nav.Link href="/ocean">Ozeanübersicht</Nav.Link>
+                <Nav.Link href="/alliances">Allianzen</Nav.Link>
+                <Nav.Link href="/players">Spieler</Nav.Link>
+                <Nav.Link href="/changes">Wechsel</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+            Welt 47
+          </Navbar>
+          <div className="root">
+            <Switch>
+              <Route path="/" exact><Home /></Route>
+              <Route path="/world" exact><World /></Route>
+            </Switch>
+          </div>
+          <Navbar fixed="bottom">
+            <Navbar.Text>
+              Made with <span className="heart">❤</span> in Austria by Floridus
+            </Navbar.Text>
+          </Navbar>
+        </Router>
+      {/*</ApolloProvider>*/}
+    </>
   );
 }
 
---
+++ src/components/World/World.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './World.css';
+
+function World () {
+  return (
+    <>
+      <div>
+        Test
+      </div>
+    </>
+  );
+}
+
+export default World;
